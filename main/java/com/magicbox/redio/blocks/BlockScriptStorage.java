0fd68935bf10ecd3be29fe119c7a9b2812622e4c
Oxygen <chenzhuoyu1992@126.com>
2014-10-12 01:50:01 +0800

    Fixed ScriptStorage placing bug since last commit.

--- main/java/com/magicbox/redio/blocks/BlockScriptStorage.java
+++ main/java/com/magicbox/redio/blocks/BlockScriptStorage.java
@@ -6,6 +6,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 import com.magicbox.redio.common.Constants;
+import com.magicbox.redio.common.Utils;
 import com.magicbox.redio.entities.EntityScriptStorage;
 
 import cpw.mods.fml.common.network.internal.FMLNetworkHandler;
@@ -33,8 +34,7 @@ public class BlockScriptStorage extends BlockBase
 	@Override
 	public boolean canPlaceBlockAt(World world, int x, int y, int z)
 	{
-		return true;// super.canPlaceBlockAt(world, x, y, z) &&
-					// Utils.hasProcessorAround(world, x, y, z);
+		return super.canPlaceBlockAt(world, x, y, z) && Utils.hasProcessorAround(world, x, y, z);
 	}
 
 	@Override
